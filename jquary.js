df4f1ed450982e1a45f75a9f87cd035030f8ed43
Md. Saiful Islam <saiful5891@gmail.com>
2020-07-01 21:21:00 +0600

    Jamil website

---
+++ jquary.js
@@ -0,0 +1,48 @@
+// $(document).ready(function () {
+
+//     $("#btn1").click(function () {
+//         $("p").hide();
+
+//     });
+//     $("#btn2").click(function () {
+//         $("p").show();
+//     })
+
+// });
+
+// ------------------------------------
+
+// $(document).ready(function () {
+
+//     $("p").click(function () {
+//         $(this).hide();
+//     });
+// });
+// -----------------------------------------
+
+// $(document).ready(function () {
+
+//     $("p").click(function () {
+//         $(this).addClass("para");
+//     });
+// });
+
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// $(document).ready(function () {
+
+//     $("p").click(function () {
+//         $(this).css({
+//             "color": "red"
+//         });
+//     });
+// });
+
+
+// ------------------------------
+$(document).ready(function () {
+
+    $("#btn2").click(function () {
+        $("p").toggle(3000);
+    });
+
+ });
